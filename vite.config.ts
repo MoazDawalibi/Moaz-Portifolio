2ed0bae22ff08cc2e234b3fedb58f5c503efb168
MoazDawalibi <moaazaldwalibi@gmail.com>
2024-11-27 19:59:49 +0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: "build",
-      rollupOptions: {
-        output: {
-          entryFileNames: "[name].[hash].js",
-          chunkFileNames: "[name].[hash].js",
-          assetFileNames: "[name].[hash][extname]",
-        },
-      },
-    },
-    envPrefix: "VITE_",
-    css: {
-      preprocessorOptions: {
-        scss: {
-          // Optionally add a global import here for variables or mixins
-          // additionalData: `@import "src/styles/variables.scss";`,
-        },
-      },
-    },
-    plugins: [
-      react(),
-    ],
-  };
-});
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// export default defineConfig(() => {
+//   return {
+//     build: {
+//       outDir: "build",
+//       rollupOptions: {
+//         output: {
+//           entryFileNames: "[name].[hash].js",
+//           chunkFileNames: "[name].[hash].js",
+//           assetFileNames: "[name].[hash][extname]",
+//         },
+//       },
+//     },
+//     envPrefix: "VITE_",
+//     css: {
+//       preprocessorOptions: {
+//         scss: {
+//           // Optionally add a global import here for variables or mixins
+//           // additionalData: `@import "src/styles/variables.scss";`,
+//         },
+//       },
+//     },
+//     plugins: [
+//       react(),
+//     ],
+//   };
+// });
